822c5c808bf6fcaea81cb506cd77c23138c401ac
ThachDfinity <75273681+ThachDfinity@users.noreply.github.com>
2021-01-07 18:40:09 -0600

    ready to push

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from aiohttp import web, MultipartReader
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+async def get_business_request(request):
+    # print(request.business)
+    return web.Response(text="Hello world", status=200)
+
+
+
+app = web.Application()
+
+app.router.add_route('GET', '/', get_business_request)
+
+web.run_app(app, host='0.0.0.0', port=6789)
