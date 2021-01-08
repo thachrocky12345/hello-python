c7b5c35f05e0681f816a428ec35dedc456afbc4c
thachrocky12345 <thachrocky@icloud.com>
2021-01-08 08:29:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config:
     FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
     RABBITMQ_ROUTING_KEY = os.environ.get('ENV2', 'test')
+    APP_INSIGHTS_CONNECTION_STRING = os.environ.get('APP_INSIGHTS_CONNECTION_STRING', 'app_insights')
