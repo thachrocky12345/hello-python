602a7e865d0c110911d11c1b9c988a0bbf16cbc3
ThachDfinity <75273681+ThachDfinity@users.noreply.github.com>
2021-01-07 19:24:57 -0600

    ready to push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app() -> Flask:
     app = Flask(__name__)
 
-    from app.controllers import register_blueprints
+    from controllers import register_blueprints
     register_blueprints(app)
 
     return app
