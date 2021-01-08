935ceb791e6dbeb026e80941cb21b5a99324537c
thachrocky12345 <thachrocky@icloud.com>
2021-01-08 08:28:13 -0500

    Update logging_lib.py

--- logging_lib.py
+++ logging_lib.py
@@ -1,12 +1,12 @@
 import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
-from constants import Constants
+from config import Config
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 import os
 import uuid
 import sys
-from opencensus.trace.tracer import Tracer
+
 
 
 def setup_logger():
@@ -15,7 +15,7 @@ def setup_logger():
     format_str = '%(asctime)s - %(levelname)-8s - %(message)s'
     date_format = '%Y-%m-%d %H:%M:%S'
     formatter = logging.Formatter(format_str, date_format)
-    handler = AzureLogHandler(connection_string=str(Constants.APP_INSIGHTS_CONNECTION_STRING))
+    handler = AzureLogHandler(connection_string=str(Config.APP_INSIGHTS_CONNECTION_STRING))
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
