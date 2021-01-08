7829c0e775ed3681e00fc0fa58f1b4bcd78b66a8
thachrocky12345 <thachrocky@icloud.com>
2021-01-08 08:33:18 -0500

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template
 import json
 bp = Blueprint('main', __name__)
 from config import Config
-
+from logging_lib import logger
 
 @bp.route('/')
 def index():
+    logger.info(f"Raw Request: {request.dict()}")
     return json.dumps({"result": "main",
                        "env": "{}-{}".format(Config.FLASK_ENV, Config.RABBITMQ_ROUTING_KEY)})
